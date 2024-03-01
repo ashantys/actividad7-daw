508fc24d6a6dadb082f8912226ff3791db605f2e
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-03-01 12:46:10 -0600

    feat(tabla grupos) creacion de atributos, claves primarias y externas

--- database/migrations/2024_03_01_182949_create_grupos_table.php
+++ database/migrations/2024_03_01_182949_create_grupos_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('grupos', function (Blueprint $table) {
-            $table->id();
+            $table->idGrupo();//Clave Primaria
+            $table->string('nivel');
             $table->timestamps();
         });
     }
