199968a275d96c9a00790f26d25f4b6e1b4541e7
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-03-01 13:07:19 -0600

    feat(creacion relacion) se creo la relacion entre cursos y kit

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Curso extends Model
 {
     use HasFactory;
+
+    public function kit()
+    {
+        return $this->hasOne(Kit::class);
+    }
+
 }
